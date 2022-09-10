52629cd6946887b3725ff0c0f454c1fea0e21ee6
Dylanson25 <dvillarreal0@ucol.mx>
2022-09-10 09:50:37 -0500

    new component field

---
+++ src/components/atoms/Field-input-atom.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-field :label="title" required>
+    <b-input v-bind="{ ...$props, ...$attrs }" v-model="text" required />
+  </b-field>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: this.value,
+    };
+  },
+  computed: {},
+  watch: {
+    value(value) {
+      this.text = value;
+    },
+  },
+  props: {
+    title: { type: String, default: "titulo" },
+    type: { type: String, default: null },
+    maxLength: { type: Number, default: null },
+    value: { type: String, default: null },
+  },
+};
+</script>
+
+<style scoped></style>
