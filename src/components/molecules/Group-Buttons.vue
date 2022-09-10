c26c6527c23a7ffc53ab2979ec7dc177f850d10e
Dylanson25 <dvillarreal0@ucol.mx>
2022-09-10 10:17:57 -0500

    new molecule group buttons

---
+++ src/components/molecules/Group-Buttons.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card-footer">
+    <Button
+      @click="() => $emit('clickDanger')"
+      :label="labelDanger"
+      type="is-danger is-light"
+      spaced
+    />
+    <Button
+      @click="() => $emit('clickSucces')"
+      :label="labelSucces"
+      type="is-success is-light"
+      spaced
+    />
+  </div>
+</template>
+
+<script>
+import { Button } from "@/components";
+export default {
+  components: {
+    Button,
+  },
+  props: {
+    labelSucces: { type: String, default: "succes" },
+    labelDanger: { type: String, default: "danger" },
+  },
+};
+</script>
+
+<style scoped>
+.card-footer {
+  display: flex;
+  padding-top: 13px;
+  justify-content: space-evenly;
+}
+</style>
