96f1feb12a3e913cf7d9aaa9739a4c4e95268f21
Dylanson25 <dvillarreal0@ucol.mx>
2022-09-10 09:34:18 -0500

    new component button

---
+++ src/components/atoms/Button-atom.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-button
+    v-bind="{ ...$props, ...$attrs }"
+    :class="classes"
+    @click="() => $emit('click')"
+    >label</b-button
+  >
+</template>
+
+<script>
+export default {
+  computed: {
+    classes() {
+      const classes = ["Button"];
+      if (this.spaced) classes.push("spaced");
+      return classes.join(" ");
+    },
+  },
+  props: {
+    type: { type: String, default: "is-success is-light" },
+    label: { type: String, default: "Accion" },
+    spaced: { type: Boolean, default: false },
+  },
+};
+</script>
+
+<style scoped>
+.spaced {
+  width: 45%;
+}
+</style>
