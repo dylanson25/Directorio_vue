cd4c083ced62f46381ab8e72fd54f15a27544050
Dylanson25 <dvillarreal0@ucol.mx>
2022-10-06 22:12:11 -0500

    add/new read directrio to show in app

--- Directorio_server/index.js
+++ Directorio_server/index.js
@@ -13,43 +13,27 @@ const { Server } = require("socket.io", {
 const io = new Server(server);
 
 let directorio = [];
-/**
- * Example:
- * directotio = { 
-  uid: 1a4650e20eb1421cb945907c7bd52012,  
-  nombre: xtfchgvj,  
-  direccion: vhbkjnlmñ,  
-  phone: 5768980965,  
-},
-{ 
-  uid: 1a4650e20eb1421cb945907c7bd52012,  
-  nombre: xtfchgvj,  
-  direccion: vhbkjnlmñ,  
-  phone: 5768980965,  
-} 
- */
+
 //dependencia necesaria para generar el archivo de texto
 const fs = require("fs");
 
 const nombreArchivo = "directorio.txt";
 
-const writeFileDirectorio = (directorioToWrite) => {
-  let datos = "";
-
-  //mapea el arreglo el cual tiene la estructura de la variable direcotrio
-  directorioToWrite.map((registro) => {
-    datos += "{ \n";
-    //A qui solo recorro las propiedades del objeto
-    for (let propName in registro) {
-      datos += `  ${propName}: ${registro[propName]},  \n`;
-    }
-    datos += "} \n";
-  });
-
-  fs.writeFile(nombreArchivo, datos, (err) => {
+const writeFileDirectorio = (data) => {
+  fs.writeFile(nombreArchivo, JSON.stringify(data), (err) => {
     if (err) throw err;
   });
   console.log("Directorio Guardado");
+};
+
+const ReadFileDirectorio = () => {
+  fs.readFile("directorio.txt", "utf-8", (err, data) => {
+    if (err) {
+      console.log("err:", err);
+    } else {
+      directorio = JSON.parse(data);
+    }
+  });
 };
 
 app.get("/", (req, res) => {
@@ -58,6 +42,9 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("A user conected");
+
+  ReadFileDirectorio();
+  socket.emit("UPDATE_DIRECTORIO", directorio);
 
   socket.on("update_directorio", (newDirectorio) => {
     console.log("Load new directorio");
